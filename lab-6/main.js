060147ba9c4a52e169a7a278c28af44f656fbb35
purv2219 <88077869+purv2219@users.noreply.github.com>
2022-03-11 21:04:41 +0530

    Add files via upload

--- lab-6/main.js
+++ lab-6/main.js
@@ -1,2 +1,10 @@
-let n = Math.floor(Math.random()*10000);
-document.body.style.backgroundColor = '#' + n;
+let variable = prompt("Enter any random number");
+let n = parseInt(variable);
+let ele  = document.getElementById("evenOrodd");
+ele.textContent = n;
+if(n % 2 == 0){
+    ele.style.backgroundColor = 'green';
+}
+else{
+    ele.style.color = 'red';
+}
